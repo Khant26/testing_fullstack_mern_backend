028b36c7ea8c5d6a49c0f4a087afe500c01c38a5
khant26 <133506936+Khant26@users.noreply.github.com>
2025-12-04 14:39:13 +0700

    Fix typo in product name from 'Contrll' to 'Control'

--- src/data/products.js
+++ src/data/products.js
@@ -4,5 +4,5 @@ export const products = [
   { id: 3, name: "Headphone", price: 100, stock: 15 },
   { id: 4, name: "Keyboard", price: 80, stock: 20 },
   { id: 5, name: "Mouse", price: 40, stock: 25 },
-  { id: 6, name: "Contrll", price: 50, stock: 10},
+  { id: 6, name: "Control", price: 50, stock: 10},
 ];
